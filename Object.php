1a2c8c14fe331806c1b0f2d211b068573df7a5f1
Bryan Joshua Pedini <b.pedini@bjphoster.com>
2020-12-29 11:16:25 +0100

    first code

---
+++ Object.php
@@ -0,0 +1,7 @@
+<?php
+    declare(strict_types=1);
+    class composer_modules {
+        public function __construct() {
+            require_once(__DIR__."/../../vendor/autoload.php");
+        }
+    }
